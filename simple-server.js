06340bb42d7142d9a04f12dfac930e46f29a0d51
javascript-2020 <76632834+javascript-2020@users.noreply.github.com>
2022-08-14 10:50:40 +0100

    Update and rename simple-server-min.js to simple-server.js

--- simple-server.js
+++ simple-server.js
@@ -1,19 +1,4 @@
 
-
-
-/*
-
-servers/simple-server/min/simple-server-min.js
-
-simple-server-min:d
-
-21-01-22
-11-08-22    v1.1
-14-08-22    min
-
-
-*/
-
   
         var scheme            = 'https';
         var port              = 3001;
